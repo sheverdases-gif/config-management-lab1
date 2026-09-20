bf3cd3e7769fe8e52399b6b1ff83ec85be5c2452
Serafima <sheverdases@gmail.com>
2026-09-20 15:56:05 +0300

    feat(config): add argument parsing and start script execution with comments support

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import argparse
 
 DEFAULT_VFS_NAME = "default_vfs"
 
@@ -33,8 +35,15 @@ def handle_input(user_input: str) -> bool:
     trimmed = user_input.strip()
     if not trimmed:
         return True
-    
+    if trimmed.startswith("#"):
+        return True
+    if "#" in trimmed:
+        trimmed = trimmed.split("#")[0].strip()
+
     tokens = trimmed.split()
+    if not tokens:
+        return True
+    
     command = tokens[0]
     args = tokens[1:]
 
@@ -45,10 +54,48 @@ def handle_input(user_input: str) -> bool:
     execute_command(command, args)
     return True
 
+def run_start_script(script_path: str) -> None:
+
+    """Читает и последовательно выполняет команды из стартового скрипта.
+    Args:
+        script_path: Физический путь к файлу скрипта на диске.
+    """
+
+    if not os.path.exists(script_path):
+        print(f"Ошибка: Стартовый скрипт не найден по пути {script_path}")
+        return
+    print(f"\n--- Выполнение стартового скрипта: {script_path} ---")
+
+    with open(script_path, "r", encoding="utf-8") as f:
+        for line in f:
+            print(f"{DEFAULT_VFS_NAME}> {line}", end="")
+
+            if not handle_input(line):
+                break
+
+    print("--- Стартовый скрипт успешно выполнени ---\n")
+
+def parse_arguments() -> argparse.Namespace:
+      
+      """Парсит аргументы командной строки при запуске приложения.
+    Returns:
+        argparse.Namespace: Объект с параметрами запуска эмулятора."""  
+      parser = argparse.ArgumentParser(description="Эмулятор командной строки UNIX-подобной ОС.")  
+      parser.add_argument("--vfs", required = True, help = "Путь к физическому расположению VFS JSON")
+      parser.add_argument("--script", required = True, help = "Путь к стартовому скрипту эмулятора")
+
+      return parser.parse_args()  
+  
 def start_repl() -> None:
     """Основная функция запуска интерактивного REPL-окружения."""
+    args = parse_arguments()
+    
     print(f"Эмулятор командной строки [23 Вариант]")
+    print(f"[DEBUG] Путь к VFS: {args.vfs}")
+    print(f"[DEBUG] Путь к скрипту: {args.script}")
     print(f'Введите "{CMD_EXIT}" для выхода.\n')
+
+    run_start_script(args.script)
 
     while True:
         show_promt()
